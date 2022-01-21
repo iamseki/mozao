102d2fe05b6b5e48c0d2b0377267eca5edda6980
iamseki <chrisyuri_19@hotmail.com>
2022-01-21 13:02:21 -0300

    startup mozao project

---
+++ styles/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  fonts: {
+    heading: 'Roboto',
+    body: 'Roboto'
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'pink.100',
+        color: 'pink.50',
+        bgGradient: 'radial(to-l, pink.200, pink.50)'
+      }
+    }
+  }
+})
